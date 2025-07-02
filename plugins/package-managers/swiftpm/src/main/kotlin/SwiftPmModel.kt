a707bd847bedd650a3855b6c87327aa7e146cfdb
Marco Lied <marco.lied@quartett-mobile.de>
2025-07-02 13:34:12 +0200

    Make detekt happy

--- plugins/package-managers/swiftpm/src/main/kotlin/SwiftPmModel.kt
+++ plugins/package-managers/swiftpm/src/main/kotlin/SwiftPmModel.kt
@@ -16,7 +16,7 @@
  * SPDX-License-Identifier: Apache-2.0
  * License-Filename: LICENSE
  */
- @file:Suppress("MaxLineLength")
+@file:Suppress("MaxLineLength")
 
 package org.ossreviewtoolkit.plugins.packagemanagers.swiftpm
 
